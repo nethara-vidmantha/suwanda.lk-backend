f37dbd0a4864d8071bb34306bbffed3c23203148
Nethara Vidmantha <xnethara@gmail.com>
2026-06-10 16:01:25 +0530

    Add user creation functionality and MongoDB connection
    - Implemented user creation logic in userController.js
    - Added userRouter for handling user-related routes
    - Established MongoDB connection in index.js
    - Updated package.json and package-lock.json to include dotenv dependency 🪄

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+import User from "../models/user.js";
+
+export function createUser(req, res) {
+    const user = new User(
+        req.body
+    )
+    user.save().then(
+        ()=>{
+            res.json({
+                message : "User created successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message : "Error creating user"
+            })
+        }
+    )
+}
+
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/userRouter.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
+const connectionString = process.env.MONGODB_URL;
+
+mongoose.connect(connectionString).then(
+    ()=>{
+        console.log("Connected to MongoDB");
+    }
+).catch(
+    ()=>{
+        console.log("Error connecting to MongoDB");
+    }
+)
+
+app.use("/users", userRouter);
 
 app.listen(5000, ()=>{
     console.log('Server is running on port 5000');
