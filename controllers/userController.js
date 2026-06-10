078fc000e75d39ebc0e2e812349531496cf6b032
Nethara Vidmantha <xnethara@gmail.com>
2026-06-10 18:05:00 +0530

    Implement user login functionality and update user creation logic 🪄

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,16 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export function createUser(req, res) {
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
     const user = new User(
-        req.body
+        {
+            email : req.body.email,
+            firstName : req.body.firstName,
+            lastName : req.body.lastName,
+            password : hashedPassword
+        }
     )
     user.save().then(
         ()=>{
@@ -19,3 +27,37 @@ export function createUser(req, res) {
     )
 }
 
+export function loginUser(req, res) {
+    User.findOne(
+        {
+            email : req.body.email
+        }
+    ).then(
+        (user)=>{
+            if(user == null){
+                res.json(
+                    {
+                        message : "User not found"
+                    }
+                )
+            }
+            else{
+                const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
+                if(isPasswordValid){
+                    res.json(
+                        {
+                            message : "Login successful"
+                        }
+                    )
+                }
+                else{
+                    res.json(
+                        {
+                            message : "Invalid password"
+                        }
+                    )
+                }
+            }
+        }
+    )
+}
